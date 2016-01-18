01c4fa580015c624fddc3364f746405d412ae97c
Walter Tan <waltertan12@gmail.com>
2016-01-17 18:45:24 -0800

    Write typing animation util and apply to landing page

--- app/frontend/components/landing_page.jsx
+++ app/frontend/components/landing_page.jsx
@@ -1,7 +1,22 @@
 const React = require('react');
 const Link = require('react-router').Link;
+const Typed = require('../utils/typed.js');
 
 var LandingPage = React.createClass({
+  componentDidMount: function () {
+    var t = new Typed('typed', [
+      'faster',
+      'yolked',
+      'strengther',
+      'ripped',
+      'a chicken wing',
+      'torn acls',
+      'yoked',
+      'strong'
+    ]);
+
+    t.typePhrases();
+  },
   render: function () {
     return (
       <div className='landing-page'>
@@ -9,7 +24,7 @@ var LandingPage = React.createClass({
           <h1>You&#39;re weak, bro...</h1>
         </div>
         <div className='enter'>
-          <h1>Get Strong</h1>
+          <h1>Get <span id='typed'>Strong</span></h1>
           <div className='center'>
             <Link to='/sign_in' className='btn btn-default'>Sign In</Link>
             <a className='btn btn-default'>Sign Up</a>
---
+++ app/frontend/utils/typed.js
@@ -0,0 +1,99 @@
+var Typed = function (id, phrases) {
+  this.el = document.getElementById(id);
+  this.phrases = phrases;
+};
+
+Typed.prototype.typePhrase = function (phrase, backspace) {   
+    var len = phrase.length,
+        timeOut,
+        letter = 0;
+
+    this.el.textContent = '|';
+
+    var _typePhrase = () => {
+      var humanize = Math.round(Math.random() * (200 - 30)) + 30;
+
+      timeOut = setTimeout(() => {
+          letter++;
+          var type = phrase.substring(0, letter);
+          this.el.textContent = type + '|';
+          _typePhrase();
+
+          if (letter == len) {
+              // remove the '|'
+              this.el.textContent = this.el.textContent.slice(0, -1);
+              if (backspace) backspace();
+              clearTimeout(timeOut);
+          }
+
+      }, humanize);
+    }
+
+    _typePhrase();
+};
+
+Typed.prototype.removePhrase = function (phrase, typeCallback) {
+  var len = phrase.length,
+      timeOut,
+      letter = len;
+
+  this.el.textContent = phrase + '|';
+
+  var _removePhrase = () => {
+    var humanize = Math.round(Math.random() * (200 - 30)) + 30;
+
+    timeOut = setTimeout(() => {
+        letter--;
+        var type = phrase.substring(0, letter);
+        this.el.textContent = type + '|';
+        _removePhrase();
+
+        if (letter == 0) {
+            // remove the '|'
+            this.el.textContent = '';
+            typeCallback();
+            clearTimeout(timeOut);
+        }
+
+    }, humanize);
+  }
+
+  _removePhrase();
+};
+
+Typed.prototype.typePhrases = function () {
+  var len = this.phrases.length,
+      phraseIndex = -1,
+      backspace, typeCallback;
+
+
+  var _typePhrases = () => {
+    if (phraseIndex < len - 1) {
+      phraseIndex++;
+
+      typeCallback = () => {
+        setTimeout(() => {
+          _typePhrases();
+        }, 500)
+      };
+
+
+      if (phraseIndex !== len - 1) {
+        console.log(this.phrases[phraseIndex]);
+        backspace = () => {
+          setTimeout(() => {
+            this.removePhrase(this.phrases[phraseIndex], typeCallback);
+          }, 500)
+        };
+      } else {
+        backspace = null;
+      }
+
+      this.typePhrase(this.phrases[phraseIndex], backspace);
+    }
+  };
+
+  _typePhrases();
+};
+
+module.exports = Typed;
