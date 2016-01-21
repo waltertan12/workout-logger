c53e8d7c05fb729010091b59128be1c7fe64dccc
Walter Tan <waltertan12@gmail.com>
2016-01-21 15:52:18 -0800

    Create users and sessions controller. Update routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: {format: :json} do
+    resources :sessions, only: [:create, :destroy]
+    resources :users
+  end
+
   root 'static_pages#index'
 end
