f1d2ac6a35f6b8e5bb67ef7254ad3a3aeb6ef554
Walter Tan <waltertan12@gmail.com>
2016-01-21 16:03:29 -0800

    Fill in UsersController#show and UsersController#create

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,8 +1,20 @@
 class Api::UsersController < ApplicationController
   def create
+    @user = User.new(user_params)
+
+    if params[:user][:password] != params[:user][:password_confirmation]
+      render json: ['Password does not match'], status: 422
+    elsif @user.save
+      log_in_user!(@user)
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
   end
 
   def show
+    @user = User.find(params[:id])
+    render json: @user
   end
 
   def update
@@ -12,5 +24,16 @@ class Api::UsersController < ApplicationController
   end
 
   def index
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(
+      :username, 
+      :email,
+      :image_url,
+      :password, 
+      :password_confirmation
+    )
   end
 end
