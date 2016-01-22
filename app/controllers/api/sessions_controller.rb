5ecff99518abf2000303a9d5390dd6485170ed3d
Walter Tan <waltertan12@gmail.com>
2016-01-21 16:04:32 -0800

    Complete SessionsController

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,18 @@
 class Api::SessionsController < ApplicationController
   def create
+    @user = User.find_by_credentials(params[:user][:email], 
+                                     params[:user][:password])
+    if @user
+      log_in_user!(@user)
+      render :show
+    else
+      render json: ['Password and/or email is incorrect'], status: 422
+    end
   end
 
   def destroy
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    render json: {}
   end
 end
