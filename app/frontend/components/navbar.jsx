d09b0b82171b42b5acd2d47cdcaa351ee0b1ceec
Walter Tan <waltertan12@gmail.com>
2016-01-22 22:11:04 -0800

    Begin building navbar

--- app/frontend/components/navbar.jsx
+++ app/frontend/components/navbar.jsx
@@ -3,9 +3,21 @@ const React = require('react');
 const Navbar = React.createClass({
   render: function () {
     return (
-      <div>
-        Navbar
-      </div>
+      <nav className='navbar navbar-custom navbar-static-top'>
+        <div className='container'>
+          <div className='navbar-header'>
+            <a className='navbar-brand' href="/#/">You're Weak Bro</a>
+          </div>
+          <ul className='nav navbar-nav'>
+            <li className=''><a href="/#/dashboard">Log</a></li>
+            <li role='separator' className='divider'/>
+            <li className=''><a href="/#/dashboard">History</a></li>
+          </ul>
+          <ul className='nav navbar-nav navbar-right'>
+            <li><a href="/#/dashboard">Hi, User</a></li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 })
--- app/frontend/containers/dashboard.jsx
+++ app/frontend/containers/dashboard.jsx
@@ -3,10 +3,21 @@ const Navbar = require('../components/navbar');
 
 const Dashboard = React.createClass({
   render: function () {
+    document.title = 'Dashboard | ' + document.title;
     return (
       <div>
         <Navbar />
-        <h1>Dashboard</h1>
+        <div className='container'>
+          <h1>Dashboard</h1>
+          <div className='row'>
+            <div className='col-md-3 exercise-pane'>
+              Exercise Pane
+            </div>
+            <div className='col-md-9 workout-pane'>
+              Current Workout Pane
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
