fff34205e4f8493a9763f283db13fc4c7fa13fe5
Walter Tan <waltertan12@gmail.com>
2016-01-22 17:16:19 -0800

    Remove comments

--- app/frontend/stores/redux_store.js
+++ app/frontend/stores/redux_store.js
@@ -3,19 +3,6 @@ const applyMiddleware =  require('redux').applyMiddleware;
 const reducer = require('../reducers/reducer');
 const thunk = require('redux-thunk');
 
-/*
-  State Structure
-  {
-    ENTER_MODAL_STATUS: null,
-    CURRENT_USER: {
-      isFetching: false,
-      id: -1,
-      username: '',
-      email: ''
-    }
-  }
-*/
-
 const createStoreWithMiddleware = applyMiddleware(
   thunk
 )(createStore);
