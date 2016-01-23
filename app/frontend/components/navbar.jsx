606b944f7b48b37d4b74fcff6e20d794e2d6d8e2
Walter Tan <waltertan12@gmail.com>
2016-01-22 22:43:11 -0800

    Style navbar

--- app/frontend/components/navbar.jsx
+++ app/frontend/components/navbar.jsx
@@ -6,15 +6,21 @@ const Navbar = React.createClass({
       <nav className='navbar navbar-custom navbar-static-top'>
         <div className='container'>
           <div className='navbar-header'>
-            <a className='navbar-brand' href="/#/">You're Weak Bro</a>
+            <a className='navbar-brand' href="/#/dashboard">
+              <img src='/assets/favicon.png' alt="You're Weak, Bro..."
+                   style={{transform: 'translateY(-5px)'}}/>
+            </a>
           </div>
           <ul className='nav navbar-nav'>
             <li className=''><a href="/#/dashboard">Log</a></li>
             <li role='separator' className='divider'/>
             <li className=''><a href="/#/dashboard">History</a></li>
+            <li className=''><a href="/#/dashboard">Friends</a></li>
+            <li className=''><a href="/#/dashboard">Graphs</a></li>
           </ul>
           <ul className='nav navbar-nav navbar-right'>
-            <li><a href="/#/dashboard">Hi, User</a></li>
+            <li><a href="/#/dashboard">Settings</a></li>
+            <li><a href="/#/dashboard">Logout</a></li>
           </ul>
         </div>
       </nav>
