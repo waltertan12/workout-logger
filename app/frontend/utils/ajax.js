531e74ee7780defa7f61f9516e047861185040b4
Walter Tan <waltertan12@gmail.com>
2016-01-30 09:22:09 -0800

    Begin building data option for #ajax method

--- app/frontend/utils/ajax.js
+++ app/frontend/utils/ajax.js
@@ -1,41 +1,42 @@
-const addCSRF = () => {
-  var send = XMLHttpRequest.prototype.send;
-  var csrfToken = document.getElementsByName('csrf-token')[0].token;
-  XMLHttpRequest.prototype.send = function(data) {
-    if ()
-    this.setRequestHeader('X-CSRF-Token', csrfToken);
-    return send.apply(this, arguments);
-  };
+const jsonToQueryString = (object) => {
+  return '?' + Object.keys(object).map((key) => {
+      return encodeURIComponent(key) + '=' +
+          encodeURIComponent(object[key]);
+  }).join('&');
 };
 
 const ajax = (options) => {
+  var params = '';
+
   if (typeof options['method'] === 'undefined')
     options.method = 'GET';
   if (typeof options['success'] === 'undefined')
     options.success = () => {};
   if (typeof options['error'] === 'undefined') 
     options.error = () => {};
+  if (typeof options['data'] !== undefined)
+    params = jsonToQueryString(options['data']);
 
   var req = new XMLHttpRequest();
 
   req.onreadystatechange = () => {
     var data, csrfToken;
     if (req.readyState === 1 && options.url.match(/^\/?api\//)) {
-      csrfToken = document.getElementsByName('csrf-token')[0].token;
+      csrfToken = document.getElementsByName('csrf-token')[0].content;
       req.setRequestHeader('X-CSRF-Token', csrfToken);
     } else if (req.readyState === 4) {
       data = JSON.parse(req.response);
       switch(req.status) {
         case 200:
           options.success(data);
-          break;
+          return req;
         default: 
           options.error(req);
-          break;
+          return req;
       }
     }
   };
-  req.open(options.method, options.url, true);
+  req.open(options.method, options.url + params, true);
   req.send();
 };
 
